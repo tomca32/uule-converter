b5ec9f063ab72971de76eebc850d245abd43afa0
Tomislav Pesut <tomca32@gmail.com>
2023-11-13 13:43:44 -0700

    Return Result instead of Option when parsing Uulev1

--- src/uulev1.rs
+++ src/uulev1.rs
@@ -1,3 +1,6 @@
+use base64_url::base64;
+use thiserror::Error;
+
 use crate::consts::{UULEV1_ROLE, UULEV1_PRODUCER};
 
 /// type alias for UULEv1 strings
@@ -16,6 +19,8 @@ pub type Uulev1 = String;
 /// # Examples
 /// ```
 /// use uule_converter::uulev1::Uulev1Data;
+/// use uule_converter::uulev1::Uulev1Error;
+/// use base64_url::base64;
 ///
 /// let uule = Uulev1Data::new("Queens County,New York,United States".to_string());
 /// assert_eq!(uule, Uulev1Data { role: 2, producer: 32, canonical_name: "Queens County,New York,United States".to_string() });
@@ -25,6 +30,15 @@ pub type Uulev1 = String;
 ///
 /// let uule = Uulev1Data::decode(&encoded).unwrap();
 /// assert_eq!(uule, Uulev1Data { role: 2, producer: 32, canonical_name: "Queens County,New York,United States".to_string() });
+///
+/// let uule = Uulev1Data::decode("asdf").unwrap_err();
+/// assert_eq!(uule, Uulev1Error::InvalidPrefix("asdf".to_string()));
+///
+/// let uule = Uulev1Data::decode("w+CAIQICIkUXVlZW5zIENvdW50eSxOZXcgWW9yayxVbml0ZWQgU3RhdGVz ").unwrap_err(); // trailing whitespace makes it invalid base64
+/// assert_eq!(uule, Uulev1Error::Base64DecodingError { source: base64::DecodeError::InvalidByte(56, 32) });
+///
+/// let uule = Uulev1Data::decode("w+CAIQICIkUXVlZW5zIENvdW50eSxOZXcgWW9yayxVbml0ZWQgU3RhdGVz").unwrap_err(); // trailing whitespace
+/// assert_eq!(uule, Uulev1Error::Base64DecodingError { source: base64::DecodeError::InvalidByte(56, 32) });
 /// ```
 #[derive(Debug, PartialEq, Clone)]
 pub struct Uulev1Data {
@@ -45,13 +59,34 @@ impl Uulev1Data {
         format!("w+{}", base64_url::encode(&bytes))
     }
 
-    pub fn decode(input: &str) -> Option<Self> {
+    pub fn decode(input: &str) -> Result<Self, Uulev1Error> {
+        if !input.starts_with("w+") {
+            return Err(Uulev1Error::InvalidPrefix(input.to_string()));
+        }
         let input = input.trim_start_matches("w+");
-        let bytes = base64_url::decode(input).ok()?;
+        let bytes = base64_url::decode(input)?;
         let role = bytes[1];
         let producer = bytes[3];
         let name_len = bytes[5] as usize;
-        let name = String::from_utf8(bytes[6..6 + name_len].to_vec()).map_err(|_| "Invalid UTF-8".to_string()).ok()?;
-        Some(Self { role, producer, canonical_name: name })
+        let name = String::from_utf8(bytes[6..6 + name_len].to_vec());
+        Ok(Self { role, producer, canonical_name: name? })
     }
+}
+
+/// Uulev1Error is an enum that represents the possible errors that can occur when decoding a UULEv1 string.
+#[derive(Error, Debug, PartialEq)]
+pub enum Uulev1Error {
+    /// Invalid prefix. UULEv1 strings must start with 'w+'. Received string is accessible as `error.0`
+    #[error("Invalid prefix. UULEv1 strings must start with 'w+'. Received: {0}")]
+    InvalidPrefix(String),
+    /// Invalid Base64-URL string. Underlying error is accessible as `error.source`
+    #[error("Invalid Base64-URL string. Underlying error: {source}")]
+    Base64DecodingError {
+        #[from] source: base64::DecodeError
+    },
+    /// Invalid UTF-8 string. Underlying error is accessible as `error.source`
+    #[error("Invalid UTF-8 string. Underlying error: {source}")]
+    Utf8DecodingError {
+        #[from] source: std::string::FromUtf8Error
+    },
 }
